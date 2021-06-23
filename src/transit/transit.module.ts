dc6d85fb16654bf337ba71cb8fe3d6fe9d6f2f0e
CrazyLittleHorse <batzotov@gmail.com>
2021-06-23 23:46:52 +0300

    Implementing post requests

--- src/transit/transit.module.ts
+++ src/transit/transit.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { TransitController } from './transit.controller';
 import { TransitService } from './transit.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [TransitController],
   providers: [TransitService],
 })
